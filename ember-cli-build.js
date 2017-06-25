f73bbb8b049dbb17cf9d96d94544227fb4034098
Jordan Vincent <contact@jordan-vincent.com>
2017-06-24 17:00:02 -0700

    fix missing fonts

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,16 +6,10 @@ module.exports = function(defaults) {
     'ember-cli-bootstrap-sassy': {
       'js': false
     },
-    
+
     fingerprint: {
       extensions: ['js', 'css', 'png', 'jpg', 'jpeg', 'gif', 'map', 'pdf']
-    },
-
-    googleFonts: [
-      'PT+Sans:400,700',
-      'PT+Serif:400,700',
-      'PT+Sans+Narrow'
-    ],
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
