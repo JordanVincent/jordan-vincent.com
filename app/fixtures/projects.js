1d02be85b3a44803c0b0f09211882efdda5d0314
Jordan Vincent <contact@jordan-vincent.com>
2017-09-12 22:21:28 -0700

    add project header, update project format

--- app/fixtures/projects.js
+++ app/fixtures/projects.js
@@ -1,30 +1,42 @@
 export default [{
   name: 'Adminca',
+  subtitle: 'A successful startup project',
   slug: 'adminca',
   date: 'from Sept. 2015',
-  summary: 'An easy to setup yet powerful interface for any backend that I co-founded.'
+  summary: 'An easy to setup yet powerful interface for any backend that I co-founded.',
+  about: 'Adminca is an admin panel as a service that is easy to setup and use. Adminca connects to the mobile development platform Parse and saves developers time and resources setting up admin panels. It is used by hundreds of mobile app agencies every month.',
+  role: 'I co-founded Adminca with Lukas Eiermann and Frank Kaiyuan Xu. I participated in every aspect of the project from business strategy to design and development.'
 },{
-  name: 'BitTitan',
+  name: 'SalesAutomation',
+  subtitle: 'Advocating for Design in building better products',
   slug: 'bittitan-ux',
   date: 'Sept. to Dec. 2015 (3 months)',
-  summary: 'Successfully demonstrated and implemented the User-Centered Design at BitTitan.'
+  summary: 'Successfully demonstrated and implemented the User-Centered Design at BitTitan.',
+  about: 'SalesAutomation is one of BitTitan’s latest products. It is a Sales application that streamlines the process of cold calling leads. The product was in developmnet for more than a year with no clear vision.',
+  role: 'I worked as one for the first UX Designers. I used the UCD process to show that design can inform product development.'
 },{
-  name: 'BitTitan Dev',
+  name: 'BitTitan',
   slug: 'bittitan-dev',
   date: 'June to Aug. 2015 (3 months)',
   summary: 'Developed a graph visualization which is now a central part of BitTitan\'s SalesAutomation.'
 },{
   name: 'SixDoors',
+  subtitle: 'My first startup experience',
   slug: 'sixdoors',
   date: '2013&ndash;14',
   position: 'Web Developer',
-  summary: "I stopped my studies at INSA of Lyon for one year to follow my dreams and work for a San Francisco's startup."
+  summary: "I stopped my studies at INSA of Lyon for one year to follow my dreams and work for a San Francisco's startup.",
+  about: 'SixDoors was an iOS app which allowed customers to shop from local stores in San Francisco and be delivered the same day. It was my first time working for a startup.',
+  role: 'I worked as a Web Developer but I was also responsible for consumer support, deliveries, and more!'
 },{
   name: 'CityDomination',
+  subtitle: 'A successful mobile game',
   slug: 'city-domination',
   date: '2011&ndash;12',
   position: 'Co-founder',
-  summary: 'A mobile game that uses geolocation to conquer the real world. Downloaded over a million times!'
+  summary: 'A mobile game that uses geolocation to conquer the real world. Downloaded over a million times!',
+  about: 'A mobile game that allows players to conquer parts of the real world. The app was downloaded over a million times!',
+  role: 'I was one of the four original founders. I participated in all the project phases, from the idea, to the launch. I was responsible for the gameplay, the graphics, and all the front-end development: the website and the web app.'
 },{
   name: 'Direct Matin',
   slug: 'direct-matin',
@@ -51,10 +63,13 @@ export default [{
   summary: 'An introduction to the visual elements of design, composition, and effective communication.'
 },{
   name: 'DinoFit',
+  subtitle: 'Exercise while playing',
   slug: 'dino-fit',
   date: '2014',
   position: 'Creator',
-  summary: 'A fitness app that keeps users motivated while doing pushups. Uses gamification to make working out fun and empowering.'
+  summary: 'A fitness app that keeps users motivated while doing pushups. Uses gamification to make working out fun and empowering.',
+  about: 'You have certainly tried in the past to work out every day. But staying motivated is challenging, it\'s much easier to sit on the couch playing on your phone. DinoFit takes a different approach, it\'s a game that requires you to do push-ups to win!',
+  role: 'I created DinoFit from scratch from visual design to development.'
 },{
   name: 'Labello',
   slug: 'labello',
@@ -62,14 +77,20 @@ export default [{
   summary: 'A web app for labelling and analyzing documents. Developed in less than a week!'
 },{
   name: 'Veritabs',
+  subtitle: 'Vertical tabs for Chrome',
   slug: 'veritabs',
   date: '2013',
   position: 'Creator',
-  summary: 'A Google Chrome plugin that helps you stay organized by displaying tabs horizontally.'
+  summary: 'A Google Chrome plugin that helps you stay organized by displaying tabs vertically.',
+  about: 'Veritabs is a Chrome extension for displaying tabs vertically. It reached a broad audience and was featured on many news website.',
+  role: 'I created this extension after a friend of mine challenged me that it wouldn\'t be possible!'
 },{
   name: 'Smart Bricks',
+  subtitle: 'LEGO sets for grown ups',
   slug: 'smart-bricks',
-  date: '2011',
+  date: '2011 - 2012',
   position: 'Creator',
-  summary: 'A web store for custom LEGO sets. I partnered with LEGO artists to distribute their creations.'
+  summary: 'A web store for custom LEGO sets. I partnered with LEGO artists to distribute their creations.',
+  about: 'Smart Bricks is a web store for custom LEGO sets. Smart Bricks first product is a beautiful 2,000 pieces set designed by a LEGO artist.',
+  role: 'I founded Smart Bricks. Designed the booklets, packaged the sets and built the web store.'
 }];
