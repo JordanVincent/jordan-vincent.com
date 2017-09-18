5188db68427fee15497aa238cc10635f9c42799d
Jordan Vincent <contact@jordan-vincent.com>
2017-09-17 22:08:59 -0700

    add next project link

--- app/fixtures/projects.js
+++ app/fixtures/projects.js
@@ -5,7 +5,8 @@ export default [{
   date: 'from June 2016',
   summary: '',
   about: 'Cogniac is a platform that allows anyone to use computer vision to solve problems. Our clients include enterprises and government agencies. Cogniac is still an early stage startup with a lot to learn and build.',
-  role: 'I am working as a Sr. UX Engineer. I designed and developed Cogniac\'s web app, marketing website and mobile apps (design only).'
+  role: 'I am working as a Sr. UX Engineer. I designed and developed Cogniac\'s web app, marketing website and mobile apps (design only).',
+  next: 'adminca'
 },{
   name: 'Adminca',
   subtitle: 'A successful startup project',
@@ -13,7 +14,8 @@ export default [{
   date: 'from Sept. 2015',
   summary: 'An easy to setup yet powerful interface for any backend that I co-founded.',
   about: 'Adminca is an admin panel as a service that is easy to setup and use. Adminca connects to the mobile development platform Parse and saves developers time and resources setting up admin panels. It is used by hundreds of mobile app agencies every month.',
-  role: 'I co-founded Adminca with Lukas Eiermann and Frank Kaiyuan Xu. I participated in every aspect of the project from business strategy to design and development.'
+  role: 'I co-founded Adminca with Lukas Eiermann and Frank Kaiyuan Xu. I participated in every aspect of the project from business strategy to design and development.',
+  next: 'bittitan-ux'
 },{
   name: 'SalesAutomation',
   subtitle: 'Advocating for Design in building better products',
@@ -21,56 +23,8 @@ export default [{
   date: 'Sept. to Dec. 2015 (3 months)',
   summary: 'Successfully demonstrated and implemented the User-Centered Design at BitTitan.',
   about: 'SalesAutomation is one of BitTitan’s latest products. It is a Sales application that streamlines the process of cold calling leads. The product was in developmnet for more than a year with no clear vision.',
-  role: 'I worked as one for the first UX Designers. I used the UCD process to show that design can inform product development.'
-},{
-  name: 'BitTitan',
-  slug: 'bittitan-dev',
-  date: 'June to Aug. 2015 (3 months)',
-  summary: 'Developed a graph visualization which is now a central part of BitTitan\'s SalesAutomation.'
-},{
-  name: 'SixDoors',
-  subtitle: 'My first startup experience',
-  slug: 'sixdoors',
-  date: '2013&ndash;14',
-  position: 'Web Developer',
-  summary: "I stopped my studies at INSA of Lyon for one year to follow my dreams and work for a San Francisco's startup.",
-  about: 'SixDoors was an iOS app which allowed customers to shop from local stores in San Francisco and be delivered the same day. It was my first time working for a startup.',
-  role: 'I worked as a Web Developer but I was also responsible for consumer support, deliveries, and more!'
-},{
-  name: 'CityDomination',
-  subtitle: 'A successful mobile game',
-  slug: 'city-domination',
-  date: '2011&ndash;12',
-  position: 'Co-founder',
-  summary: 'A mobile game that uses geolocation to conquer the real world. Downloaded over a million times!',
-  about: 'A mobile game that allows players to conquer parts of the real world. The app was downloaded over a million times!',
-  role: 'I was one of the four original founders. I participated in all the project phases, from the idea, to the launch. I was responsible for the gameplay, the graphics, and all the front-end development: the website and the web app.'
-},{
-  name: 'Direct Matin',
-  slug: 'direct-matin',
-  date: 'June to Aug. 2012 (3 months)',
-  summary: 'Developed a web app for a national newspaper while interning at Kreactive. Seen in Paris\' subway & newspapers.'
-},{
-  name: 'Haiku Deck',
-  slug: 'haiku-deck',
-  date: 'Jan. to Mar. 2015',
-  summary: 'User testing of the presentation tool Haiku Deck. Carried out a study with eight participants.',
-},{
-  name: 'StartupViz',
-  slug: 'startup-viz',
-  date: 'Sept. to Dec. 2014',
-  summary: 'An interactive visualization of the US startup universe. Presented at the University of Washington\'s Startup Hall.',
-  about: 'StartupViz is an interactive visualization of the US startup universe. Using the CrunchBase dataset, StartupViz allows you to make your own discoveries. You can explore everything from general market trends to funding rounds for a particular startup.'
-},{
-  name: 'Bridge Disability Ministry',
-  slug: 'bridge-disability-ministry',
-  date: 'Apr. to June 2015',
-  summary: 'Redesign and development of a website for a non-profit organization.'
-},{
-  name: 'Visual Communication',
-  slug: 'visual-communication',
-  summary: 'An introduction to the visual elements of design, composition, and effective communication.',
-  about: 'Visual Communication gives an introduction to the visual elements of design, organizational principles of composition, and effective and meaningful communication. It was one of my favorite class at the University of Washington. Here are some of my assignments.'
+  role: 'I worked as one for the first UX Designers. I used the UCD process to show that design can inform product development.',
+  next: 'dino-fit'
 },{
   name: 'DinoFit',
   subtitle: 'Exercise while playing',
@@ -79,23 +33,8 @@ export default [{
   position: 'Creator',
   summary: 'A fitness app that keeps users motivated while doing pushups. Uses gamification to make working out fun and empowering.',
   about: 'You have certainly tried in the past to work out every day. But staying motivated is challenging, it\'s much easier to sit on the couch playing on your phone. DinoFit takes a different approach, it\'s a game that requires you to do push-ups to win!',
-  role: 'I created DinoFit from scratch from visual design to development.'
-},{
-  name: 'Labello',
-  slug: 'labello',
-  date: 'Feb. 2015',
-  summary: 'A web app for labelling and analyzing documents. Developed in less than a week!',
-  about: 'Labello is a web app for labelling and analyzing documents. For a Qualitative User Research course at the University of Washington, I needed an effective tool to analyze lengthy transcribed interviews.',
-  role: 'I couldn\'t find an easy way to do it, so I developed Labello in just a few nights!'
-},{
-  name: 'Veritabs',
-  subtitle: 'Vertical tabs for Chrome',
-  slug: 'veritabs',
-  date: '2013',
-  position: 'Creator',
-  summary: 'A Google Chrome plugin that helps you stay organized by displaying tabs vertically.',
-  about: 'Veritabs is a Chrome extension for displaying tabs vertically. It reached a broad audience and was featured on many news website.',
-  role: 'I created this extension after a friend of mine challenged me that it wouldn\'t be possible!'
+  role: 'I created DinoFit from scratch from visual design to development.',
+  next: 'smart-bricks'
 },{
   name: 'Smart Bricks',
   subtitle: 'LEGO sets for grown-ups',
@@ -104,5 +43,85 @@ export default [{
   position: 'Creator',
   summary: 'A web store offering custom LEGO sets. I partnered with an artist to distribute his creations.',
   about: 'Smart Bricks is a web store for custom LEGO sets. Smart Bricks\'s first product is a beautiful 2,000 pieces set designed by a LEGO artist.',
-  role: 'I founded Smart Bricks. I didn\'t created the LEGO model by myself, but I created a 3D model, designed the booklets, packaged the sets, and developed the web store.'
+  role: 'I founded Smart Bricks. I didn\'t created the LEGO model by myself, but I created a 3D model, designed the booklets, packaged the sets, and developed the web store.',
+  next: 'sixdoors'
+},{
+  name: 'SixDoors',
+  subtitle: 'My first startup experience',
+  slug: 'sixdoors',
+  date: '2013&ndash;14',
+  position: 'Web Developer',
+  summary: "I stopped my studies at INSA of Lyon for one year to follow my dreams and work for a San Francisco's startup.",
+  about: 'SixDoors was an iOS app which allowed customers to shop from local stores in San Francisco and be delivered the same day. It was my first time working for a startup.',
+  role: 'I worked as a Web Developer but I was also responsible for consumer support, deliveries, and more!',
+  next: 'veritabs'
+},{
+  name: 'Veritabs',
+  subtitle: 'Vertical tabs for Chrome',
+  slug: 'veritabs',
+  date: '2013',
+  position: 'Creator',
+  summary: 'A Google Chrome plugin that helps you stay organized by displaying tabs vertically.',
+  about: 'Veritabs is a Chrome extension for displaying tabs vertically. It reached a broad audience and was featured on many news websites.',
+  role: 'I created this extension after a friend challenged me that it wouldn\'t be possible!',
+  next: 'city-domination'
+},{
+  name: 'CityDomination',
+  subtitle: 'A successful mobile game',
+  slug: 'city-domination',
+  date: '2011&ndash;12',
+  position: 'Co-founder',
+  summary: 'A mobile game that uses geolocation to conquer the real world. Downloaded over a million times!',
+  about: 'A mobile game that allows players to conquer parts of the real world. The app was downloaded over a million times!',
+  role: 'I was one of the four original founders. I participated in all the project phases, from the idea, to the launch. I was responsible for the gameplay, the graphics, and all the front-end development: the website and the web app.',
+  next: null
+},
+
+// Unlisted
+{
+  name: 'BitTitan',
+  slug: 'bittitan-dev',
+  date: 'June to Aug. 2015 (3 months)',
+  summary: 'Developed a graph visualization which is now a central part of BitTitan\'s SalesAutomation.',
+  next: null
+},{
+  name: 'Direct Matin',
+  slug: 'direct-matin',
+  date: 'June to Aug. 2012 (3 months)',
+  summary: 'Developed a web app for a national newspaper while interning at Kreactive. Seen in Paris\' subway & newspapers.',
+  next: null
+},{
+  name: 'Haiku Deck',
+  slug: 'haiku-deck',
+  date: 'Jan. to Mar. 2015',
+  summary: 'User testing of the presentation tool Haiku Deck. Carried out a study with eight participants.',
+  next: null
+},{
+  name: 'StartupViz',
+  slug: 'startup-viz',
+  date: 'Sept. to Dec. 2014',
+  summary: 'An interactive visualization of the US startup universe. Presented at the University of Washington\'s Startup Hall.',
+  about: 'StartupViz is an interactive visualization of the US startup universe. Using the CrunchBase dataset, StartupViz allows you to make your own discoveries. You can explore everything from general market trends to funding rounds for a particular startup.',
+  role: 'StartupViz is a class project in collabioration with Lukas Eiermann, Alison Chambliss, and Aripta Kulkarni. I participated in every phase of the project.',
+  next: null
+},{
+  name: 'Bridge Disability Ministry',
+  slug: 'bridge-disability-ministry',
+  date: 'Apr. to June 2015',
+  summary: 'Redesign and development of a website for a non-profit organization.',
+  next: null
+},{
+  name: 'Visual Communication',
+  slug: 'visual-communication',
+  summary: 'An introduction to the visual elements of design, composition, and effective communication.',
+  about: 'Visual Communication gives an introduction to the visual elements of design, organizational principles of composition, and effective and meaningful communication. It was one of my favorite class at the University of Washington. Here are some of my assignments.',
+  next: null
+},{
+  name: 'Labello',
+  slug: 'labello',
+  date: 'Feb. 2015',
+  summary: 'A web app for labelling and analyzing documents. Developed in less than a week!',
+  about: 'Labello is a web app for labelling and analyzing documents. For a Qualitative User Research course at the University of Washington, I needed an effective tool to analyze lengthy transcribed interviews.',
+  role: 'I couldn\'t find an easy way to do it, so I developed Labello in just a few nights!',
+  next: null
 }];
