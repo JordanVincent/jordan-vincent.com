d2eb6ac14be7496ebf0c47ff9add8e9cc981c7f4
Jordan Vincent <contact@jordan-vincent.com>
2019-09-04 09:49:47 -0700

    Update charts

--- app/routes/us-flights.js
+++ app/routes/us-flights.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   headData: Ember.inject.service(),
 
-  // afterModel() {
-  //   this.set('headData.description', 'Explore overnight stays at US National Parks and uncover surprising patterns.');
-  //   this.set('headData.image', 'http://jordan-vincent.com/assets/images/meta-image-nps-nights.png');
-  // },
+  afterModel() {
+    this.set('headData.description', 'What does the United States look like from major airports?');
+    this.set('headData.image', 'http://jordan-vincent.com/assets/images/meta-image-us-flights.png');
+  },
 
   title() {
     return 'A flight against time | Jordan Vincent';
