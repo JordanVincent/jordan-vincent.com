02e3100edb3a08977ee5cfa62641303e9aeb3a1f
Jordan Vincent <contact@jordan-vincent.com>
2019-10-12 16:41:56 -0700

    Force desktop vesion on mobile
    Making the us-flight page responsive would be too much work...

--- app/routes/us-flights.js
+++ app/routes/us-flights.js
@@ -3,9 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   headData: Ember.inject.service(),
 
-  afterModel() {
+  activate() {
     this.set('headData.description', 'What does the United States look like from major airports?');
     this.set('headData.image', 'http://jordan-vincent.com/assets/images/meta-image-us-flights.png');
+    // Request desktop view on mobile
+    this.set('headData.viewport', 'width=1000');
+  },
+
+  deactivate() {
+    this.set('headData.viewport', null);
   },
 
   title() {
